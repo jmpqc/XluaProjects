f45bb0178670518704411b628d017bfa9f3089ae
jmpqc <3138335779@qq.com>
2019-06-03 19:05:36 +0800

    2.0.1 增加新鱼

---
+++ FishingJoy/Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class LoadAssets {
+    public static Dictionary<string, GameObject> prefabDict = new Dictionary<string, GameObject>();
+    public static void LoadAsset(string prefabName, string filePath)
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile(@"C:\Users\dream\Desktop\XLuaActualCombat\XluaProjects\FishingJoy\AssetBundles\" + filePath);
+        GameObject gameObject = ab.LoadAsset<GameObject>(prefabName);
+        prefabDict.Add(prefabName, gameObject);
+    }
+
+    public static GameObject GetGameObject(string prefabName)
+    {
+        return prefabDict[prefabName];
+    }
+}
---
+++ FishingJoy/Assets/Scripts/LoadAssetsForLua.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using XLua;
+
+[Hotfix]
+public class LoadAssetsForLua : MonoBehaviour {
+
+	// Use this for initialization
+    [LuaCallCSharp]
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
