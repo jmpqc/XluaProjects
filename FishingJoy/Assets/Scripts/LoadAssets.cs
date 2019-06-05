0a7fe530644898bc3c90f7b2f06297bc438b60b7
jmpqc <3138335779@qq.com>
2019-06-05 18:29:29 +0800

    V2.x completed

--- FishingJoy/Assets/Scripts/LoadAssets.cs
+++ FishingJoy/Assets/Scripts/LoadAssets.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Networking;
 
-public static class LoadAssets {
-    public static Dictionary<string, GameObject> prefabDict = new Dictionary<string, GameObject>();
-    public static void LoadAsset(string prefabName, string filePath)
+public class LoadAssets : MonoBehaviour
+{
+    public Dictionary<string, GameObject> prefabDict = new Dictionary<string, GameObject>();
+
+
+    public void LoadAsset(string prefabName, string filePath)
     {
-        AssetBundle ab = AssetBundle.LoadFromFile(@"C:\Users\dream\Desktop\XLuaActualCombat\XluaProjects\FishingJoy\AssetBundles\" + filePath);
+        StartCoroutine(LoadResourceCorotine(prefabName, filePath));
+    }
+
+    IEnumerator LoadResourceCorotine(string prefabName, string filePath)
+    {
+        UnityWebRequest request = UnityWebRequest.GetAssetBundle(@"http://127.0.0.1/AssetBundles/" + filePath);
+        yield return request.SendWebRequest();//从服务器下载完后，继续执行下面的代码
+        AssetBundle ab = (request.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
         GameObject gameObject = ab.LoadAsset<GameObject>(prefabName);
         prefabDict.Add(prefabName, gameObject);
     }
 
-    public static GameObject GetGameObject(string prefabName)
+    public GameObject GetGameObject(string prefabName)
     {
         return prefabDict[prefabName];
     }
+
 }
